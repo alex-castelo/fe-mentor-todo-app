e9809e57049d7376b897df60a25b682a193ec21a
Àlex Castelo Martínez <alexcastelomartinez@gmail.com>
2023-12-23 15:06:12 +0100

    set a default todos if todos length is zero

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import TodoInput from "../../components/TodoInput";
@@ -6,9 +6,34 @@ import TodoList from "../../components/TodoList/TodoList";
 
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 
+const defaultTodos = [
+  {
+    description: "See your first React course",
+    id: uuidv4(),
+    completed: true,
+  },
+  {
+    description: "Create your first React application",
+    id: uuidv4(),
+    completed: true,
+  },
+  {
+    description: "Keep exploring and learning",
+    id: uuidv4(),
+    completed: false,
+  },
+];
+
 const Home = () => {
   const [todos, setTodos] = useLocalStorage("todos", []);
   const [todo, setTodo] = useState("");
+
+  useEffect(() => {
+    if (todos.length === 0) {
+      setTodos(defaultTodos);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const addTodo = (description) => {
     const todo = {
