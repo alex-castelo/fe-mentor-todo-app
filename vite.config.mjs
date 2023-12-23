37256449bf46e40ca8f5b9262d684a11b26857fa
Àlex Castelo Martínez <alexcastelomartinez@gmail.com>
2023-12-23 14:01:18 +0100

    update project base url

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/fe-mentor-todo-app/",
   plugins: [react()],
   server: {
     port: 3000,
